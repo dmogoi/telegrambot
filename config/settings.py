e3e122f24112f15357c2a7b61a26aa5b4fdb50ec
Dickson <147909717+dmogoi@users.noreply.github.com>
2025-03-12 10:23:28 -0700

    added 'STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')' in settings

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,8 @@ STATIC_URL = 'static/'
 # settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
