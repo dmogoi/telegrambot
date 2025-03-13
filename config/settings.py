22b5f79159287f6acb843ee8b025013c44a474f5
Dickson <147909717+dmogoi@users.noreply.github.com>
2025-03-13 01:20:50 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
 
 #CELERY_BROKER_URL = 'redis://localhost:6379/0'
 #CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_BROKER_URL = 'redis://bot-redis.redis.render.com:6379/0'
-CELERY_RESULT_BACKEND = 'redis://bot-redis.redis.render.com:6379/0'
+CELERY_BROKER_URL = 'redis://red-cv998kd2ng1s73d1o3f0:6379/0'
+CELERY_RESULT_BACKEND = 'redis://red-cv998kd2ng1s73d1o3f0:6379/0'
 
 
 # Password validation
